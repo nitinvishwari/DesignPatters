8789cf735b353cc4a93158f3ebfb3d83f2e254ad
nitinvishwari <71441089+nitinvishwari@users.noreply.github.com>
2024-08-18 21:53:21 -0700

    singleton design pattern

--- design/src/main/java/com/nitin/designPatterns/Singleton.java
+++ design/src/main/java/com/nitin/designPatterns/Singleton.java
@@ -1,5 +1,25 @@
 package com.nitin.designPatterns;
 
-public class Singleton {
+class Bird{
 	
+	String name;
+	float weight;
+	
+	static Bird bird = new Bird();
+	
+	private Bird() {
+	}
+	
+	public static Bird getInstance() {
+		return bird;
+	}
+}
+
+public class Singleton {
+	public static void main(String[] args){
+		Bird birdObject = Bird.getInstance();
+		birdObject.name = "parrot";
+		birdObject.weight = (float) 2.5;
+		System.out.println(birdObject.name + ", " + birdObject.weight + "pounds");
+	}
 }
