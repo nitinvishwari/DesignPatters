e62047da232623a092b316b7cf4a4a343cc0b52b
nitinvishwari <71441089+nitinvishwari@users.noreply.github.com>
2025-07-09 20:57:29 -0700

    Bird class

--- design/src/main/java/Practise2025/SOLID.java
+++ design/src/main/java/Practise2025/SOLID.java
@@ -2,6 +2,28 @@ package Practise2025;
 
 public class SOLID {
 	public static void main(String[] args) {
-		System.out.println("Nitin is here");
+		Bird bird = new Bird("parrot", true);
+		System.out.println(bird.whatIsYourName());
 	}
 }
+
+class Bird{
+	String name;
+	boolean fly;
+	
+	public Bird(String name, boolean fly) {
+		this.name = name;
+		this.fly = fly;
+	}
+	
+	public boolean doYouFly() {
+		return this.fly;
+	}
+	
+	public String whatIsYourName() {
+		return this.name;
+	}
+}
+
+
+
