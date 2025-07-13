471836a0dd9b6d51e2c6ee6b8b0f68a4c289602e
nitinvishwari <71441089+nitinvishwari@users.noreply.github.com>
2025-07-13 14:22:18 -0700

    Updating the java version and also adding one class for thread lessson

--- design/src/main/java/Practise2025/JavaPractise/ThreadCreation.java
+++ design/src/main/java/Practise2025/JavaPractise/ThreadCreation.java
@@ -1,5 +1,8 @@
 package Practise2025.JavaPractise;
 
 public class ThreadCreation {
-
+	public static void main(String[] args) {
+		Thread currentThread = Thread.currentThread();
+		System.out.println(currentThread.getClass());
+	}
 }
