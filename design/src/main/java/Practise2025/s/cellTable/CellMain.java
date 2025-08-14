fdc1abf263f1c9ffbf2919961d7d7596f4b61d6e
nitinvishwari <71441089+nitinvishwari@users.noreply.github.com>
2025-08-14 09:34:20 -0700

    Cell Problem update

--- design/src/main/java/Practise2025/s/cellTable/CellMain.java
+++ design/src/main/java/Practise2025/s/cellTable/CellMain.java
@@ -1,74 +1,86 @@
 package Practise2025.s.cellTable;
 
-public class CellMain {
+import java.util.*;
+
+class CellKey{
+	int row;
+	int col;
 	
-}
-
-
-
-/*
-Schema 
-Cell Table
-primary key(excelId*rowId*cellId), value
-
-interface cellTable{
-	boolean setCellValue(int row, int column, int value)
-	int getCellValue(int row, int column)
-}
-
-interface printCellTable{
-	printFirstNRows(int n)
+	CellKey(int row, int col){
+		this.row = row;
+		this.col = col;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(!(o instanceof CellKey)) {
+			return false;
+		}
+		CellKey cellObject = (CellKey) o;
+		if(cellObject.row == this.row && cellObject.col == this.col) {
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(row, col);
+	}
+	
+	@Override
+	public String toString() {
+		return "row" + row + "col" + col;
+	}
 }
 
 class Cell{
-	int row;
-	int coloumn;
-	int value;
-	
-	// getter setter for this
-}
-
-class CellImplemenation implements cellTable{
-	HashMap<row@Column, Cell> cellMap;
-	
-	public CellImplentation(HashMap<row@Column, Cell> inputMap){
-		this.cellMap = inputMap;
+	Integer value;
+	String formula;
+	List<Cell> dependentList;
+	public Cell() {
+		this(null, null);
 	}
-	
-	public boolean setCellValue(int row, int column, int value){
-		
+	public Cell(String formula) {
+		this(null, formula);
 	}
-}
-
-class PrintImplementation implements printCellTable{
-	HashMap<row@Column, Cell> cellMap;
-	
-	public PrintImplementation(HashMap<row@Column, Cell> inputMap){
-		this.cellMap = inputMap;
+	public Cell(Integer value) {
+		this(value, null);
 	}
-	
-	public void printFirstNRows(int n){
-		//keep on checking columns till you are getting the best one
+	public Cell(Integer value, String formula) {
+		this.formula = formula;
+		this.value = value;
+		dependentList = new ArrayList<>();
+	}
+	public void setValue(Integer value) {
+		this.value = value;
 	}
 }
 
-class GetCellTable{
-	//factory design pattern as their can be many implementation for cellTable and print
-	//
-}
- 
-class Main{
-	psvm(){
-		cellTable = GetCellTable.getInstance("firstImplementation");
-		printTable = GetCellTable.getInstance("firstPrintImplementation)
+class SpreadSheet{
+	
+	int columnCount;
+	HashMap<CellKey, Cell> cellMap;
+	
+	public SpreadSheet() {
+		columnCount = 0;
+		cellMap = new HashMap<>();
+	}
+	
+	public void setValueOrRowCol(int row, int col, String value) {
+		if(value.matches("\\d+")) {
+			CellKey cellKey = new CellKey(row, col);
+			if(cellMap.containsKey(cellKey)) {
+				Cell cell = cellMap.get(cellKey);
+				System.out.println(cell.)
+			}
+		}
 	}
 }
 
-
- 
-
-
-
-
-
-*/
+class CellMain{
+	public static void main(String[] args) {
+		CellKey ck = new CellKey(1, 2);
+		System.out.println(ck.toString());
+	}
+}
