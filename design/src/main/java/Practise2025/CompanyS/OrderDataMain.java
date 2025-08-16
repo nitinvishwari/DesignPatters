ce79d2fea2caacc9012e1b69c68e319675b3137d
nitinvishwari <71441089+nitinvishwari@users.noreply.github.com>
2025-08-16 15:36:39 -0700

    Order Data and Permission Data Structure

---
+++ design/src/main/java/Practise2025/CompanyS/OrderDataMain.java
@@ -0,0 +1,22 @@
+package Practise2025.CompanyS;
+
+/*
+
+Goal: Transform/join datasets for order-related records.
+Requirements:
+Implement operations like:
+
+
+Join orders with clients (inner/outer)
+
+
+Filter, sort, or aggregate based on keys
+
+
+Handle missing data cases
+
+ */
+
+public class OrderDataMain {
+
+}
---
+++ design/src/main/java/Practise2025/CompanyS/PermissionDataMain.java
@@ -0,0 +1,23 @@
+package Practise2025.CompanyS;
+
+/*
+Goal: Design a permission system with hierarchy.
+Requirements:
+If a user has permission on /a, they have access to all sub-paths.
+
+
+APIs:
+
+
+grant_permission(user, path)
+
+
+check_permission(user, path)
+
+
+Inheritance support, efficient lookup
+*/
+
+public class PermissionDataMain {
+	
+}
