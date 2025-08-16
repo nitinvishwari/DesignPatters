e459f42acaf002cc903e1729722aa39c3eab8ffa
nitinvishwari <71441089+nitinvishwari@users.noreply.github.com>
2025-08-16 15:33:13 -0700

    Json parse pending

---
+++ design/src/main/java/Practise2025/CompanyS/JsonParserMain.java
@@ -0,0 +1,67 @@
+package Practise2025.CompanyS;
+
+import java.util.*;
+
+/*
+Support:
+
+Nested objects and arrays
+Strings, numbers, booleans, null
+
+Avoid built-in parsers (e.g., json.loads)
+
+Must handle malformed JSON
+
+String Json = "{\"Name\": \"value\", ......}"
+*/
+
+
+abstract class AnyObject{
+	public AnyObject() {}
+}
+
+class JsonObject extends AnyObject{
+	
+}
+
+class BooleanType extends AnyObject{
+	
+}
+
+class StringType extends AnyObject{
+	
+}
+
+class NumberType extends AnyObject{
+	
+}
+
+class ArrayType extends AnyObject{
+	
+}
+
+class JsonParser{
+	
+	public JsonParser(){};
+	
+	public Map<String, Node> parse(String parsingString){
+		return null;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+public class JsonParserMain {
+	public static void main(String[] args) {
+		
+	}
+}
