3c29443fac1f198c1b8e87a1fb228cd4023770d9
nitinvishwari <71441089+nitinvishwari@users.noreply.github.com>
2025-08-16 18:10:42 -0700

    Solved Partially

--- design/src/main/java/Practise2025/CompanyS/OrderDataMain.java
+++ design/src/main/java/Practise2025/CompanyS/OrderDataMain.java
@@ -1,22 +1,122 @@
 package Practise2025.CompanyS;
 
+import java.util.*;
+
 /*
 
 Goal: Transform/join datasets for order-related records.
 Requirements:
 Implement operations like:
-
-
-Join orders with clients (inner/outer)
-
-
-Filter, sort, or aggregate based on keys
+1.Join orders with clients (inner/outer)
+2. Filter, sort, or aggregate based on keys
 
 
 Handle missing data cases
+UserTable
+	UserId
+	UserInformation
+
+
+Order Table
+	OrderId 
+	OrderDetail 
+	UserId
+
 
  */
 
-public class OrderDataMain {
+class Order{
+	String orderId;
+	String orderDetails;
+	String userId;
+	
+	public Order(String orderId, String orderDetails, String userId) {
+		this.orderId = orderId;
+		this.orderDetails = orderDetails;
+		this.userId = userId;
+	}
+	
+	@Override
+	public boolean equals(Object order) {
+		if(!(order instanceof Order)) {
+			return false;
+		}
+		Order inputOrder = (Order) order;
+		if(inputOrder.orderId.equals(this.orderId)) {
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.orderId);
+	}
+	
+	@Override
+	public String toString() {
+		return orderId + " " + orderDetails + " " + userId;
+	}
 
+}
+
+class User{
+	String userId;
+	String mail;
+	
+	public User(String userId, String mail) {
+		this.userId = userId;
+		this.mail = mail;
+	}
+	
+	@Override
+	public boolean equals(Object user) {
+		if(!(user instanceof User)) {
+			return false;
+		}
+		User inputUser = (User) user;
+		if(inputUser.userId.equals(this.userId)) {
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.userId);
+	}
+	
+	@Override
+	public String toString() {
+		return userId + " " + mail;
+	}
+}
+
+class OrderDao{
+	HashSet<Order> orderSet;
+	
+	public OrderDao() {
+		orderSet = new HashSet<>();
+	}
+	
+	public boolean addOrder(Order order) {
+		if(orderSet.contains(order)) {
+			return false;
+		}
+		orderSet.add(order);
+		return true;
+	}
+	
+//	public Set<Order> filterBy(Set<Order> inputOrderSet){
+//		HashSet<Order> afterFilterSet = new HashSet<>();
+//		for(Order order: inputOrderSet) {
+//			if()
+//		}
+//	}
+}
+
+public class OrderDataMain {
+	public static void main(String[] args) {
+		
+	}
 }
