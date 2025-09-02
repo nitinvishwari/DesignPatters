9be0d7e618634497c3aa03068753fa056c60d320
nitinvishwari <71441089+nitinvishwari@users.noreply.github.com>
2025-09-01 20:22:57 -0700

    filter implemented

--- design/src/main/java/Practise2025/JavaPractise/Streams.java
+++ design/src/main/java/Practise2025/JavaPractise/Streams.java
@@ -1,23 +1,21 @@
 package Practise2025.JavaPractise;
 
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 public class Streams {
 	
 	record Car(String name, String brand, int price) {}
 	
 	public static void main(String[] args) {
-		Car car = new Car("Camry", "Toyota", 42000);
-		HashSet<Car> hs = new HashSet<>();
-		hs.add(car);
-		if(hs.contains(new Car("Camry", "Toyota", 42000))) {
-			System.out.println("Key Matched");
-		}
-		else {
-			System.out.println("Key Not Matched");
-		}
-		System.out.println(car);
+		List<Car> list = new ArrayList<>();
+		Car car1 = new Car("Camry", "Toyota", 42000);
+		Car car2 = new Car("Corolla", "Toyota", 32000);
+		Car car3 = new Car("Model 3", "Tesla", 38000);
+		list.add(car1);
+		list.add(car2);
+		list.add(car3);
+		List<Car> filteredCars = list.stream().filter((a) -> a.brand.equals("Toyota")).toList();
+		System.out.println(filteredCars);
 	}
 	
 }
